344e72df47e93ee1a700c61bc5cd7b07faf0876e
silviocesarjunior <silvio_czar_cpv@hotmail.com>
2023-04-27 19:15:23 -0300

    list
    list variables

---
+++ Lista/script.js
@@ -0,0 +1,31 @@
+let listElement = document.querySelector("#app ul");
+let inputElement = document.querySelector("#app input");
+let buttonElement = document.querySelector("#app button");
+
+let tarefas = [];
+
+function renderTarefas(){
+    listElement.innerHTML = "";
+    tarefas.map((teste) =>{
+        let liElement = document.createElement("li");
+        let tarefaText = document.createTextNode(teste);
+
+        liElement.appendChild(tarefaText);
+        listElement.appendChild(liElement);
+    })
+}
+
+function adicionarTarefas(){
+    if(inputElement.value === ''){
+        alert("Digite alguma tarefa");
+        return false;
+    }else{
+        let novaTarefa = inputElement.value;
+
+        tarefas.push(novaTarefa);
+        inputElement.value = '';
+        renderTarefas();
+    }
+}
+
+buttonElement.onclick = adicionarTarefas;
