7b7d05bc43046d130bee08cae3d71e6d776d22fa
silviocesarjunior <silvio_czar_cpv@hotmail.com>
2023-04-27 21:44:33 -0300

    save localstorage
    saving

--- Lista/script.js
+++ Lista/script.js
@@ -2,7 +2,7 @@ let listElement = document.querySelector("#app ul");
 let inputElement = document.querySelector("#app input");
 let buttonElement = document.querySelector("#app button");
 
-let tarefas = [];
+let tarefas = JSON.parse(localStorage.getItem("@listaTarefas")) || [];
 
 function renderTarefas(){
     listElement.innerHTML = "";
@@ -10,10 +10,22 @@ function renderTarefas(){
         let liElement = document.createElement("li");
         let tarefaText = document.createTextNode(teste);
 
+        let linkElement = document.createElement("a");
+        linkElement.setAttribute("href", "#");
+
+        let linkText = document.createTextNode(" Excluir");
+        linkElement.appendChild(linkText);
+
+        let posicao = tarefas.indexOf(teste);
+        linkElement.setAttribute("onclick", `deletarTarefa(${posicao})`)
+
         liElement.appendChild(tarefaText);
+        liElement.appendChild(linkElement);
         listElement.appendChild(liElement);
     })
 }
+
+renderTarefas();
 
 function adicionarTarefas(){
     if(inputElement.value === ''){
@@ -25,7 +37,18 @@ function adicionarTarefas(){
         tarefas.push(novaTarefa);
         inputElement.value = '';
         renderTarefas();
+        salvarDados();
     }
 }
 
 buttonElement.onclick = adicionarTarefas;
+
+function deletarTarefa(posicao){
+    tarefas.splice(posicao, 1);
+    renderTarefas();
+    salvarDados();
+}
+
+function salvarDados(){
+    localStorage.setItem("@listaTarefas", JSON.stringify(tarefas))
+}
